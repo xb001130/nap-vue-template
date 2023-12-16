09b37fbab8b5cb72c35f3259f60684bf50353bf6
xiaobin <xiaob3634@gmail.com>
2023-12-16 23:01:26 +0800

    config ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+      ],
+      dts: 'types/auto-imports.d.ts',
+    }),
+    Components({
+      dts: 'types/components.d.ts',
+    }),
+  ],
 })
