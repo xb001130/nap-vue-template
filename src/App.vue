e0c6b3fea8d382ba57665a35a83c6073ac7c6700
xiaobin <xiaob3634@gmail.com>
2023-12-22 00:53:33 +0800

    add dark transition

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,60 @@ async function toggleLocales() {
 }
 
 const isDark = useDark()
-const toggleDark = useToggle(isDark)
+// const toggleDark = useToggle(isDark)
+// @ts-expect-error: Transition API
+const isAppearanceTransition = document.startViewTransition
+  && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+function toggleDark() {
+  if (!isAppearanceTransition || !event) {
+    isDark.value = !isDark.value
+    return
+  }
+  const x = (event as any).clientX
+  const y = (event as any).clientY
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  )
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  })
+  transition.ready.then(() => {
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${endRadius}px at ${x}px ${y}px)`,
+    ]
+    document.documentElement.animate(
+      {
+        clipPath: isDark.value
+          ? [...clipPath].reverse()
+          : clipPath,
+      },
+      {
+        duration: 400,
+        easing: 'ease-in',
+        pseudoElement: isDark.value
+          ? '::view-transition-old(root)'
+          : '::view-transition-new(root)',
+      },
+    )
+  })
+}
 </script>
 
 <template>
-  <div font-rubik>
-    {{ t('test') }}
-    Nap
-    <i class="i-carbon-3d-mpr-toggle" />
+  <div flex justify-center>
+    <div font-rubik>
+      {{ t('test') }}
+      Nap
+      <i class="i-carbon-3d-mpr-toggle" />
+    </div>
+    <div i-carbon-language font-size-10 @click="toggleLocales()" />
+    <div i="carbon-sun dark:carbon-moon" font-size-10 @click="toggleDark()" />
   </div>
-  <div i-carbon-language font-size-10 @click="toggleLocales()" />
-  <div i="carbon-sun dark:carbon-moon" font-size-10 @click="toggleDark()" />
 </template>
 
 <style scoped>
