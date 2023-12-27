5854ea046603071b9c8f93ea40c678ba3f9d55eb
xiaobin <xiaob3634@gmail.com>
2023-12-27 10:00:55 +0800

    update: tsconfig.json

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { useAppStoreHook } from '~/store/modules/app'
+
+const projectName = computed(() => useAppStoreHook().projectName)
 </script>
 
 <template>
   <div flex-center flex-col>
     <div i-nap:napt h20 font-size-30 />
-    <div>[pinia] {{ useAppStoreHook().projectName }}</div>
+    <div>[pinia] {{ projectName }}</div>
   </div>
 </template>
 
